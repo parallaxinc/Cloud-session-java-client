7b75d3d0c6b81163aa54e1200bc0bf9a6ec5a5ed
michel-cf <michel@creatingfuture.eu>
2015-07-27 22:50:29 +0200

    Get user info by email address

---
+++ src/main/java/com/parallax/client/cloudsession/CloudSessionUserService.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.parallax.client.cloudsession;
+
+import com.github.kevinsawicki.http.HttpRequest;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.parallax.client.cloudsession.exceptions.UnknownUserException;
+import com.parallax.client.cloudsession.objects.User;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Michel
+ */
+public class CloudSessionUserService {
+
+    private final String BASE_URL;
+
+    public CloudSessionUserService(String baseUrl) {
+        this.BASE_URL = baseUrl;
+    }
+
+    public User getUser(String email) throws UnknownUserException {
+        Map<String, String> data = new HashMap<>();
+        data.put("email", email);
+        HttpRequest request = HttpRequest.get(getUrl("user/email/" + email));
+//        int responseCode = request.code();
+//        System.out.println("Response code: " + responseCode);
+        String response = request.body();
+//        System.out.println(response);
+        JsonElement jelement = new JsonParser().parse(response);
+        JsonObject responseObject = jelement.getAsJsonObject();
+        if (responseObject.get("success").getAsBoolean()) {
+            JsonObject userJson = responseObject.get("user").getAsJsonObject();
+            User user = new User();
+            user.setId(userJson.get("id").getAsLong());
+            user.setEmail(userJson.get("email").getAsString());
+            user.setLocale(userJson.get("locale").getAsString());
+            user.setScreenname(userJson.get("screenname").getAsString());
+            return user;
+        } else {
+            String message = responseObject.get("message").getAsString();
+            switch (responseObject.get("code").getAsInt()) {
+                case 400:
+                    throw new UnknownUserException(email, message);
+            }
+            System.out.println(response);
+            return null;
+        }
+    }
+
+    private String getUrl(String actionUrl) {
+        return BASE_URL + actionUrl;
+    }
+
+}
