bd3d7db315bc26a996cbbe4c7b13db8b765d119d
michel-cf <michel@creatingfuture.eu>
2015-08-15 22:12:46 +0200

    Change user info

--- src/main/java/com/parallax/client/cloudsession/CloudSessionUserService.java
+++ src/main/java/com/parallax/client/cloudsession/CloudSessionUserService.java
@@ -10,6 +10,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.parallax.client.cloudsession.exceptions.UnknownUserException;
+import com.parallax.client.cloudsession.exceptions.UnknownUserIdException;
 import com.parallax.client.cloudsession.objects.User;
 import java.util.HashMap;
 import java.util.Map;
@@ -49,6 +50,35 @@ public class CloudSessionUserService {
             switch (responseObject.get("code").getAsInt()) {
                 case 400:
                     throw new UnknownUserException(email, message);
+            }
+            System.out.println(response);
+            return null;
+        }
+    }
+
+    public User changeUserInfo(Long idUser, String screenname) throws UnknownUserIdException {
+        Map<String, String> data = new HashMap<>();
+        data.put("screenname", screenname);
+        HttpRequest request = HttpRequest.post(getUrl("user/info/" + idUser)).form(data);
+//        int responseCode = request.code();
+//        System.out.println("Response code: " + responseCode);
+        String response = request.body();
+//        System.out.println(response);
+        JsonElement jelement = new JsonParser().parse(response);
+        JsonObject responseObject = jelement.getAsJsonObject();
+        if (responseObject.get("success").getAsBoolean()) {
+            JsonObject userJson = responseObject.get("user").getAsJsonObject();
+            User user = new User();
+            user.setId(userJson.get("id").getAsLong());
+            user.setEmail(userJson.get("email").getAsString());
+            user.setLocale(userJson.get("locale").getAsString());
+            user.setScreenname(userJson.get("screenname").getAsString());
+            return user;
+        } else {
+            String message = responseObject.get("message").getAsString();
+            switch (responseObject.get("code").getAsInt()) {
+                case 400:
+                    throw new UnknownUserIdException(idUser, message);
             }
             System.out.println(response);
             return null;
